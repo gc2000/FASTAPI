389c5f9a0091c1f391734424930a5c8983f699e8
Gu Chao <guchao2000@gmail.com>
2024-02-11 07:22:58 +0000

    change-20240211-1

--- orm/database.py
+++ orm/database.py
@@ -3,11 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgres://postgres:postgres@db1/db1"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres/db1"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
+    )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
