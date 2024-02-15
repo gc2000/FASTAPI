b1d7e1a71a8392995b0114c2312920717af3ae7f
Gu Chao <guchao2000@gmail.com>
2024-02-15 06:45:58 +0000

    Pull request - JWT

--- orm/routers/auth.py
+++ orm/routers/auth.py
@@ -1,18 +1,20 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oauth2
 from ..database import get_db
 
 
 router = APIRouter(tags= ['Authentication'])
 
 @router.post('/login')
-def login(user_credential: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credential.email).first()
+def login(user_credential: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credential.username).first()
     if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{user_credential.email} not found")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"{user_credential.email} not found")
     
     if not utils.verify_password(user_credential.password, user.password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Wrong credential")
     
-    return {"msg": "success"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
